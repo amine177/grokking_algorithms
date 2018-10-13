0346317b5d401df7dbf2cb96ab6d059422341f75
aminos <amri.amine@protonmail.com>
2018-10-13 13:03:07 +0100

    a FIFO queue

---
+++ ex_2.2_queue.py
@@ -0,0 +1,84 @@
+# !/bin/env python3
+# -*- coding: utf-8 -*-
+
+import random
+
+
+class Node:
+    """A class representing a node in a linked list"""
+
+    def __init__(self, value, next):
+        """Node(value, next)
+        Arguments:
+            value: the value
+            next: the next node
+        """
+
+        self.value = value
+        self.next = next
+
+
+class Queue:
+    """A class representing a FIFO queue"""
+
+    def __init__(self, max):
+        """Queue(max)
+        Arguments:
+            max: the queue's capacity
+        """
+
+        self.max = max
+        self.number = 0
+        self.list = None
+
+    def push(self, value):
+        """pushes a value to the queue
+        Arguments:
+            value: the value to push
+        """
+
+        if self.list is None:
+            self.list = Node(value, None)
+        else:
+            if self.number + 1 > self.max:
+                raise Exception("queue is full")
+            n = self.list
+            while (n.next is not None):
+                n = n.next
+            n.next = Node(value, None)
+        self.number += 1
+
+    def pop(self):
+        """popes a value from the queue
+        Arguments:
+            None
+        """
+
+        if self.number == 0:
+            return None
+
+        n = self.list
+        self.list = self.list.next
+        self.number -= 1
+        return n.value
+
+    def isempty(self):
+        """returns True if the queue is empty false otherwise"""
+        return self.number == 0
+
+    def isfull(self):
+        """returns True if the queue is full, Fasle otherwise"""
+        return self.number == self.max
+
+    def __len__(self):
+        """returns the number of elements in the queue"""
+        return self.number
+
+
+if __name__ == "__main__":
+
+    q = Queue(300)
+    while not q.isfull():
+        q.push(random.random() * 5)
+    while not q.isempty():
+        print("q.pop() = {}".format(q.pop()))
