29c99168cdd1301aa9d3f270572233b095831346
aminos <amri.amine@protonmail.com>
2018-10-13 04:15:19 +0100

    a binary search module

---
+++ ch1/binary_search.py
@@ -0,0 +1,35 @@
+# !/bin/env python3
+
+
+def search(l, el):
+    """
+        A function that applies binary search
+        on a sorted indexed object.\n
+        l: sorted indexed object\n
+        el: element\n
+    """
+
+    s = 0
+    e = len(l) - 1
+    m = (e - s) // 2
+    while (s < e):
+        if el > l[m]:
+            s = m + 1
+        elif el < l[m]:
+            e = m - 1
+        else:
+            return m
+
+        m = (e - s // 2)
+
+    return None
+
+
+if __name__ == "__main__":
+
+    r = range(0, 5500)
+
+    i = search(r, 557)
+    if i is not None:
+        print("Element found at {:d}"
+              .format(i))
