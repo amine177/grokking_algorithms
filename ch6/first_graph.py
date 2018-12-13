d02f54a5365d21eafcee555ba864486115197c2d
aminos <amri.amine@protonmail.com>
2018-12-13 17:51:10 +0100

    implementing a graph through a hashmap

---
+++ ch6/first_graph.py
@@ -0,0 +1,10 @@
+# !/bin/env python
+# -*- coding: utf-8 -*-
+
+
+if __name__ == "__main__":
+    g = dict()
+    g["q1"] = ["q2", "q3"]
+    g["q2"] = ["q1", "q4"]
+    g["q3"] = ["e"]
+    g["q4"] = ["e", "q3"]
